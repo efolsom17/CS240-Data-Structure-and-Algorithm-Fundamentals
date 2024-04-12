6019ca7910920347ae1ae9ef55d02e10c8f44643
efolsom17 <eric.folsom17@gmail.com>
2024-04-11 17:02:30 -0700

    Made some progress on Data structures Assignment 1 and did some testing. Mostly did some reading to understand creating custom classes and object types in Python. My back hurts so I am going home

--- Assignments/Data Structures Assignment 1/Data Structures Assignment 1 - Eric Folsom.py
+++ Assignments/Data Structures Assignment 1/Data Structures Assignment 1 - Eric Folsom.py
@@ -3,7 +3,11 @@
 
 # Reading the data used to verify implementation
 
+with open("./Data/numbers-2.txt", "r") as nums: #opens the file
+    numbers2 = [int(line.strip()) for line in nums] #extract contents line by line and store as an integer.
 
+## Smaller list of numbers to test with before I implement the full numbers-2.txt data.
+numbers_test = list(range(0,100)) # need to make sure to define as a list, python doesnt do this automatically like R does.
 
 
 
--- Assignments/Data Structures Assignment 1/Internet testing.py
+++ Assignments/Data Structures Assignment 1/Internet testing.py
@@ -2,7 +2,8 @@
 ## will probably delete things later.
 
 #Singly Linked list implementation in Python
-
+# This is a generic, pre-defined linked list where we define the values of the list when we initialize the list class
+# Will need to make something similar to this that allows me to read in a list of numbers (ints).
 
 class Node:
     # Creating a node
@@ -21,11 +22,13 @@ if __name__ == '__main__':
 
     linked_list = LinkedList()
 
+
+    ## Going to need this section and the section where I connect nodes to be dynamic based on the size of my input list
     # Assign item values
     # In my code, make it so you can dynamically assign values given the input being a list.
-    linked_list.head = Node(1)    # Something like linked_list.head = Node(value of index 1)
-    second = Node(2)     # second = Node (value of index 2)
-    third = Node(3)    # third = Node (value of index 3)
+    linked_list.head = Node("item")    # Something like linked_list.head = Node(value of index 1)
+    second = Node("item")     # second = Node (value of index 2)
+    third = Node("item")    # third = Node (value of index 3)
 
     # Connect nodes
     linked_list.head.next = second
