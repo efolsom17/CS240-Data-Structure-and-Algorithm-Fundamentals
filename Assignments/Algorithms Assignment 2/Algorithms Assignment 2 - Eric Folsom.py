10d3a1e2efba93d1da453c1a4f5d7b5adc8a7ca2
efolsom17 <eric.folsom17@gmail.com>
2024-04-15 23:19:49 -0700

    Implemented insertion sort.

--- Assignments/Algorithms Assignment 2/Algorithms Assignment 2 - Eric Folsom.py
+++ Assignments/Algorithms Assignment 2/Algorithms Assignment 2 - Eric Folsom.py
@@ -1,23 +1,40 @@
 ## Algorithms Assignment 2 - Eric Folsom ##
 
+
+# for testing purposes, randomizing my testing sample
 from random import sample as sample
-from random import randint as rdunif
+from random import seed as setSeed
+
+setSeed(12345) # reproducible randomization
+
+# Testing Array
+
+test_numbs = sample(list(range(100)), 100)
 
 ## Importing the numbers-1.txt data
 
 with open("./Data/numbers-1.txt", "r") as nums: #opens the file
     numbers1 = [int(line.strip()) for line in nums]
 
-test_numbs = list(range(100))
-test_numbs = sample(test_numbs, len(test_numbs))
+
 
 '''
 Selction Sort
+
+We repeat the following process $n-1$ times where $n$ is the length of the array. This is because once there is only one element left, we can assume that it is the largest element of the array.
+
+1. Assign the first unsorted element as the minimum value of the array.
+2. For every other unsorted element in the array:
+   1. Compare the value of the element with the value we assigned as the minimum
+        * If the value of the element is less than the minimum value we assigned:
+          * Set this value as the new minimum value
+3. Swap the positions of the minimum value with the first unsorted element. 
+
 '''
 def SelectionSort(array, asc = True, desc = False):
     # array: array of values to be sorted, assumes each element is an int
     # asc: sort in ascending order, Default: True
-    # desc: sort in descending order, Default: True
+    # desc: sort in descending order, Default: False
     
     array = array[:] # so that we don't manipulate the original array.
     
@@ -47,4 +64,45 @@ def SelectionSort(array, asc = True, desc = False):
        
 '''
 Insertion Sort
+
+1. Assume that the fist element, $x_0$ is sorted into it's proper position.
+2. For each unsorted element in $X$:
+   1. Store the value of $x_i$.
+      1. For each of the indices from the last sorted index down to $0$ ($x_j$ through $x_0$):
+        * If the $x_j$ is greater than $x_i$:
+          * Move $x_j$ to the right by one, $x_j = x_{j+1}$.
+        * Insert $x_i$ into the array at the index $j$
 '''
+
+def InsertionSort(array, asc = True, desc = False):
+    # array: array of values to be sorted, assumes each element is an int
+    # asc: sort in ascending order, Default: True
+    # desc: sort in descending order, Default: False
+    
+    array = array[:] # so that we don't manipulate the original array, will just print the sorted array
+    
+    if desc == True: # decsending
+        for i in range(1,len(array)): # starting at index 1 because we assume index 0 is already sorted
+            
+            x_i = array[i] # The value we are comparing against
+            j = i-1 # the highest index in the array that is sorted, starts at 0
+            
+            # Search sorted indices until we reach the correct position of x_i
+            while j >= 0 and array[j] < x_i: 
+                array[j + 1] = array[j]  # Shift elements to the right
+                j -= 1 # Go to the next sorted element
+            array[j+1] = x_i # insert the stored value $x_i$
+    
+    else: # ascending
+        for i in range(1,len(array)): # starting at index 1 because we assume index 0 is already sorted
+            
+            x_i = array[i] # The value we are comparing against
+            j = i-1 # the highest index in the array that is sorted, starts at 0
+            
+            # Search sorted indices until we reach the correct position of x_i
+            while j >= 0 and array[j] > x_i: 
+                array[j + 1] = array[j]  # Shift elements to the right
+                j -= 1 # Go the the next sorted element
+            array[j+1] = x_i # insert the stored value $x_i$
+            
+    return array # return the sorted array.
