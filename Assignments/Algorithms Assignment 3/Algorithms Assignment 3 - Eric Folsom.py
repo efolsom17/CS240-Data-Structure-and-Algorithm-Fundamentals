48748b0bcbdd990683e19563cf43d68af462005c
efolsom17 <eric.folsom17@gmail.com>
2024-04-22 14:59:59 -0700

    added question 5, and updated the naming of the numbers-3.txt data file.

--- Assignments/Algorithms Assignment 3/Algorithms Assignment 3 - Eric Folsom.py
+++ Assignments/Algorithms Assignment 3/Algorithms Assignment 3 - Eric Folsom.py
@@ -1,7 +1,7 @@
 ## Importing the numbers-3.txt data
 
 with open("./Data/numbers-3.txt", "r") as nums: #opens the file
-    numbers1 = [int(line.strip()) for line in nums]
+    numbers3 = [int(line.strip()) for line in nums]
     
 from random import sample
 
