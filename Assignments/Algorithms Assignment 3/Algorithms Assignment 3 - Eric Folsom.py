1634fd1a3c15fa5c697575b4411bb26b35182eab
efolsom17 <eric.folsom17@gmail.com>
2024-04-23 17:28:48 -0700

    Implemented a recursive binary search function and cleaned some stuff up.

--- Assignments/Algorithms Assignment 3/Algorithms Assignment 3 - Eric Folsom.py
+++ Assignments/Algorithms Assignment 3/Algorithms Assignment 3 - Eric Folsom.py
@@ -31,5 +31,37 @@ def LinSearchRec(data, value):
   
   searchindex(0) # search starting the list starting at index 0
   
-LinSearchIt(test, 100)
-LinSearchRec(test, 100)
+def BinarySearchRec(data, item = int, low = 0, high = None):
+  
+  #set high to be the highest index in the array/list
+  if high is None: # default, will set the last index as the highest index
+    high = len(data)-1
+
+  if low > high: # if we are outside the bounds of the list 
+    return "Item Not Found"
+  
+  mid = (high+low) //2 # define the middle index
+
+  if data[mid] == item: # base case, we find the item at the middle index
+    return print(f"{item} is located at index {mid}") #tell us where the item is
+
+  elif item < data[mid]: # if our item we are searching for is less than the middle item
+    return BinarySearchRec(data, item, low, mid-1) # search the left(bottom) half of the list
+    
+  else: # if the item we ar seraching for is greater than the middle item
+    return BinarySearchRec(data, item, mid+1, high) #search the right(top) half of the list.
+
+
+
+#### testing ####  
+LinSearchIt(test, 10)
+LinSearchRec(test, 10)
+
+from cs240functions import InsertionSort
+from cs240functions import binary_search
+
+test = InsertionSort(test)
+
+LinSearchRec(test,10)
+BinarySearchRec(test,10)
+binary_search(10,test)
