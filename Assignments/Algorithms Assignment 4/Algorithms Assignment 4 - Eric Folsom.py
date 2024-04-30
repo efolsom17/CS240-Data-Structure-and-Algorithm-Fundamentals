3cb75e747b66f7829321b90dd48c8613874d04ca
efolsom17 <eric.folsom17@gmail.com>
2024-04-29 21:34:50 -0700

    Started the python script and the quarto a little more

---
+++ Assignments/Algorithms Assignment 4/Algorithms Assignment 4 - Eric Folsom.py
@@ -0,0 +1,18 @@
+### CS240 Algorithms Assignment 4 - Eric Folsom ###
+
+'''
+Quick Sort was a lot easier for me to wrap my head around than merge sort. I was able to get
+a good idea on how merge sort was but it would be a lot easier to explain my understanding in a visual form
+versus a written form I am afraid. Thinking about the recursion stack as an absolute value function graph
+really helped me get a grasp on what was happening in the algorithm and helped me with my pseudocode and implementation.
+'''
+
+from cs240functions import binary_search as BinarySearch
+from random import sample as sample
+
+
+## Merge Sort 
+
+
+
+## Quick Sort
