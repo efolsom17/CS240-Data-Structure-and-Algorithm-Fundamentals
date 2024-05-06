7c8bdae60c81da64e1e194a0f2091a5a3e016625
efolsom17 <eric.folsom17@gmail.com>
2024-05-06 08:26:59 -0700

    Getting started on the hash table implementation.

--- Assignments/Data Structures Assignment 3/Data Structures Assignment 3 - Eric Folsom.py
+++ Assignments/Data Structures Assignment 3/Data Structures Assignment 3 - Eric Folsom.py
@@ -1,11 +1,20 @@
-## Hashing function 
+## Data Structures Assignment 3 - Eric Folsom ##
 
-def HashFunc(string, base = int, mod = int):
-    hash = 0 # start at 0
-    for char in string: # for each character in the string
-        hash = ((hash * base) + ord(char)) % mod # compute the hash value for character, stops when we are at the last value
-    return hash # return the index for the hash table
-
+'''
+Implementing a hash table in python. Think that I need to do two different implementations of a hash table
+which should be on the different ways to handle collisinos. Want to do a version which implements chaining
+and another which implements linear probing. Chaining seems to be the easiest method to start with, deciding on either a linked list or an array.
+'''
 
 class HashTable:
-    pass
+    def __init__(self, size):
+        self.size = size
+        self.table = [] * size
+        
+        
+    def HashFunc(string, base = int, mod = int):
+        hash = 0 # start at 0
+        for char in string: # for each character in the string
+            hash = ((hash * base) + ord(char)) % mod # compute the hash value for character, stops when we are at the last value
+        return hash # return the index for the hash table
+    
