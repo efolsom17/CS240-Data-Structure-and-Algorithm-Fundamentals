232d71fa5794d277135b31771cd5d0f86acc64d4
efolsom17 <eric.folsom17@gmail.com>
2024-05-07 16:58:11 -0700

    Got Started on Hanoi's tower

---
+++ Assignments/Midterm/HanoisTower.py
@@ -0,0 +1,16 @@
+# Hanoi's Tower implementation in python
+# Would Like to use stacks for this implementaion:
+# Took some inspiration for implementing this on how I have seen other people implement this algorithm, 
+# But would like to modify the implementations to store the states of the tower as separate stacks. 
+# To make it clearer, I would like each rod to be its own stack and we will move items in the stack (discs)
+# between stacks to demonstrate the actual tower of hanoi problem with physical rods and discs. I will store numbers
+# in the stacks which will represent the radius of a disc. So each stack will look like as follows at the beginning if we had 3 discs to move
+# [1]   []      []
+# [2]   []      []
+# [3]   []      []
+#
+# Which after runnning the algorithm would look like:
+# []    []      [1]
+# []    []      [2]
+# []    []      [3]
+
