b6d673592e53989de119e4c83af19346266030ec
efolsom17 <eric.folsom17@gmail.com>
2024-05-06 18:07:06 -0700

    Downloaded a thing that contains the whole english language idk. Also some work done on the hash table

--- Assignments/Data Structures Assignment 3/Data Structures Assignment 3 - Eric Folsom.py
+++ Assignments/Data Structures Assignment 3/Data Structures Assignment 3 - Eric Folsom.py
@@ -3,9 +3,14 @@
 '''
 Implementing a hash table in python. Think that I need to do two different implementations of a hash table
 which should be on the different ways to handle collisinos. Want to do a version which implements chaining
-and another which implements linear probing. Chaining seems to be the easiest method to start with, deciding on either a linked list or an array.
+and another which implements linear probing. Chaining seems to be the easiest method. Going to use a linked list.
 '''
 
+# double linked list from cs240functions
+from cs240functions import DoubleLinkedList as DoubleLinkedList
+
+
+#Hash table
 class HashTable:
     
     #Initializing hash table
@@ -13,8 +18,14 @@ class HashTable:
         self.size = size # set the size to be the user defined size
         self.table = [] * size #build an empty hash table of the user defined size.
         
-    # Function for hashing values, built in algorithms assignment 5.    
-    def HashFunc(string, base = 31, mod = int): # I think I want mod to be dependent on the size of the table, going to use 31 for base as I have seen it other places
+    # function to get a prime number to use with our modulo in the hashing, want to be based on the size 
+    # of the hash table, to resolve collisions
+    
+        
+    # Function for hashing values, built in algorithms assignment 5. (slightly modified so I can modify the mod size)     
+    def HashFunc_Horner(string): 
+        base = 31 #going to use 31 for base as I have seen it other places when I looked up what horner's method is
+        mod = None# I think I want mod to be dependent on the size of the table.
         hash = 0 # start at 0
         for char in string: # for each character in the string
             hash = ((hash * base) + ord(char)) % mod # compute the hash value for character, stops when we are at the last value
