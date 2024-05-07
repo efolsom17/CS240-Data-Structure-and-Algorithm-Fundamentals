c0e9b2efef0c3d71525bf0beb4049d45dcb8c58b
efolsom17 <eric.folsom17@gmail.com>
2024-05-06 20:51:56 -0700

    Good Progress

--- Assignments/Data Structures Assignment 3/Data Structures Assignment 3 - Eric Folsom.py
+++ Assignments/Data Structures Assignment 3/Data Structures Assignment 3 - Eric Folsom.py
@@ -23,7 +23,7 @@ class HashTable:
     # of the hash table, should in theory limit collisions
     # I got this code and the idea to implement something like this from programiz 
     
-    def _isPrime(sefl,n): # Checking if a number is prime (has more than itself and 1 as its divisors and is greater than 1)
+    def _isPrime(self,n): # Checking if a number is prime (has more than itself and 1 as its divisors and is greater than 1)
         if n == 1 or n == 0: # if the number is 1 or 0
             return False # not a prime, must be greater than 1
         for i in range(2, n//2): # Checks if the number is divisble by every number up to n/2, any more than that is pointless
@@ -106,11 +106,71 @@ class HashTable:
 # ( I am going to need this on the midterm so I might as well do it now)
 
 class HashTableString:
-    pass
- 
-#testing
+    #Initializing hash table
+    def __init__(self, size):
+        self.size = size # set the size to be the user defined size
+        self.table = [None] * size #build an empty hash table of the user defined size.
+        
+    # functions to get a prime number to use with our modulo in the hashing, want to be based on the size 
+    # of the hash table, should in theory limit collisions
+    # I got this code and the idea to implement something like this from programiz 
+    
+    def _isPrime(self,n): # Checking if a number is prime (has more than itself and 1 as its divisors and is greater than 1)
+        if n == 1 or n == 0: # if the number is 1 or 0
+            return False # not a prime, must be greater than 1
+        for i in range(2, n//2): # Checks if the number is divisble by every number up to n/2, any more than that is pointless
+            if n % i == 0: # if the remainder is 0, i.e, n is divisble by i
+                return False # the number is not a prime
+        # if we make it all the way through and the above has not been satisfied
+        return True # then the number is prime
 
-   
+    # This function gets the prime number closest to the size of the hash table.
+    def _getPrime(self,n): # Assumed that n is the size of the hash table so we will work downwards
+        if n % 2 == 0: # Check if n is even ( even numbers are not prime)
+            n = n - 1 # ensure that n is odd
+        while not self._isPrime(n): # call the isPrime function above to check if the number is prime, while this is not true
+            n -=2 # go to the next odd number, 
+        # once isPrime(n) == True
+        return n # return the prime number
+
+        
+    # Function for hashing values, Using summation method that I saw on w3schools    
+    def HashFunction(self, string): 
+        base = 31 #going to use 31 for base as I have seen it other places when I looked up what horner's method is
+        mod = self._getPrime(self.size)# I think I want mod to be dependent on the size of the table. Mod using above two helper functions
+        sum_char = 0 # starts at 0, sum of the charachters in the string's unicode values
+        for char in string: # for each character in the string,
+            sum_char += ord(char) # add the unicode value
+        hash = sum_char % mod # hash value sum of the unicode characters mod prime number smaller than hash size
+        return hash # return the hash value
+    
+    # function to insert a key into the hash table
+    def insert(self, key):
+        # get the index of the bucket that the key should go in
+        index = self.HashFunction(key)
+        # if the bucket does not exist, create it
+        if self.table[index] == None:
+            self.table[index] = []
+        # else (or after we initialize the bucket), add the item to the bucket
+        self.table[index].append(key)
+    
+    # function to check if a key is present in the hash table
+    def contains(self, key):
+        # get the hash that the key should be
+        # check if the key is there
+        pass
+    
+    # funciton to remove a key from the hash table
+    
+    # some ways to display the hash table
+    def display(self):
+        return self.table
+ 
+
+
+
+#testing
+  
 words = ["The" ,"Quick" ,"Brown" ,"Fox" ,"Jumped", "Over", "the", "Lazy" ,"Dog"]
 test = HashTableString(10)
 for word in words:
