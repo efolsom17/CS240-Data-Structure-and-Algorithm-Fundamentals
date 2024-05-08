768ecfe69a98a23971df67cc218aa9d354a093e6
efolsom17 <eric.folsom17@gmail.com>
2024-05-07 21:32:32 -0700

    Got started on the spell checker problem.

---
+++ Assignments/Midterm/SpellCheck.py
@@ -0,0 +1,47 @@
+## Spell checker program, needs to be CLI and stores the dictionary as a hash table. 
+from cs240functions import HashTable as HashTable
+
+'''
+Must do the following:
+
+1. Load a dictionary of words from a file into the hash table.  You will need to create this yourself.  Aim for 20 to 100 words. (oops I did 1000 words for my dictionary) 
+
+2. Take a string of text as input and check each word in the text against the words in the dictionary stored in the hash table. Your program should identify any words that are not found in the dictionary and display them as "misspelled".
+
+3. Implement a suggestion feature that suggests possible correct spellings for the misspelled words based on edit distance.
+
+4. Implement a Levenshtein distance algorithm to compute the edit distance between two words.
+
+5. Allow the user to add new words to the dictionary and update the hash table accordingly.
+
+6. Handle collisions in the hash table using separate chaining.
+
+7. Optimize the performance of your program in terms of time and space complexity.
+
+8. Your program should be user-friendly and have a command-line interface.
+
+'''
+
+
+# loading the dictionary, 1000 most common words in the english language and loading it into a hash table.
+
+n = 1000 # hash table size
+dictionary = HashTable(n)
+with open("./Data/dictionary.txt", "r") as words:
+    words = [line.strip() for line in words]
+    
+for word in words:
+    dictionary.insert(word)
+
+del words
+dictionary.display()
+    
+#  fucntion to take a string of text and check each word in the text against the words in the hash table and identify any "misspellings".
+
+def Misspell(string):
+    temp = string.split()# split the string into each word, store it as a temp variable
+    spellings = []# created an empty array the length of the temp variable ( call it spellings)
+    # for each word in the temp variable:
+    #   run dictionary.get(word) and append the spellings array with the truth value
+    # return the spellings array
+    pass
