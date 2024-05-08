5e63feff6d2a5643aecf43ee5b6b9c2997787212
efolsom17 <eric.folsom17@gmail.com>
2024-05-07 22:23:13 -0700

    Some Changes and observations I have noticed along the way. # returning just the string truth value doesn't work, because if anything is true it exits the CLI loop.

--- Assignments/Midterm/SpellCheck.py
+++ Assignments/Midterm/SpellCheck.py
@@ -6,7 +6,11 @@ Must do the following:
 
 1. Load a dictionary of words from a file into the hash table.  You will need to create this yourself.  Aim for 20 to 100 words. (oops I did 1000 words for my dictionary) 
 
+EaSY
+
 2. Take a string of text as input and check each word in the text against the words in the dictionary stored in the hash table. Your program should identify any words that are not found in the dictionary and display them as "misspelled".
+
+Did this, returns an array with the True if a word is misspelled and False if a word is correctly Spelled.
 
 3. Implement a suggestion feature that suggests possible correct spellings for the misspelled words based on edit distance.
 
@@ -16,25 +20,29 @@ Must do the following:
 
 6. Handle collisions in the hash table using separate chaining.
 
+Chains using an array to store collisions.
+
 7. Optimize the performance of your program in terms of time and space complexity.
 
 8. Your program should be user-friendly and have a command-line interface.
+
+
 
 '''
 
 
 # loading the dictionary, 1000 most common words in the english language and loading it into a hash table.
 
+
 n = 1000 # hash table size
 dictionary = HashTable(n)
 with open("./Data/dictionary.txt", "r") as words:
-    words = [line.strip() for line in words]
-    
+    words = [line.strip() for line in words] 
 for word in words:
     dictionary.insert(word)
-
 del words
-dictionary.display()
+
+
     
 #  fucntion to take a string of text and check each word in the text against the words in the hash table and identify any "misspellings".
 
@@ -51,4 +59,43 @@ def Misspell(string):
 
 '''
 Okay how do I create a CLI program?????
+
+using the cmd module because it seems like its the friendliest to work with.
 '''
+
+import cmd
+
+class SpellCheck(cmd.Cmd):
+    intro = "~~~~~ Spell Check Program ~~~~~\n Input a string with  '' "
+    prompt = "(spellcheck)"
+    def do_exit(self, arg):
+        'Exit the shell'
+        print("Exiting")
+        return True  # return True to stop the cmd loop
+ 
+    def __init__(self):
+        super().__init__()  # Initialize the Cmd superclass
+        # loading the dictionary, 1000 most common words in the english language and loading it into a hash table.
+        n = 1000 # hash table size
+        dictionary = HashTable(n)
+        with open("./Data/dictionary.txt", "r") as words:
+            words = [line.strip() for line in words] 
+        for word in words:
+            dictionary.insert(word)
+        del words
+        
+    
+    def _misspelled(self, string):
+        temp = string.split()# split the string into each word, store it as a temp variable
+        spellings = []# created an empty array the length of the temp variable ( call it spellings)
+        for word in temp:# for each word in the temp variable:
+         #   run dictionary.contains(word) and append the spellings array with the truth value
+            spellings.append(not(dictionary.contains(word)))
+        # return the spellings array
+        return spellings
+    
+    def do_spellcheck(self, string):
+        
+        return self._misspelled(string) # returning just the string doesn't work, because if anything is true it exits the CLI loop.
+
+    
