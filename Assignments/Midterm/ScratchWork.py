a414a6c1256ddd501561d3948f0ca6a4c6efcb5d
efolsom17 <eric.folsom17@gmail.com>
2024-05-09 18:17:49 -0700

    Got the Tower of Hanoi Problem to work recursively with stacks!

--- Assignments/Midterm/ScratchWork.py
+++ Assignments/Midterm/ScratchWork.py
@@ -63,26 +63,55 @@ def tower_of_hanoi(n, starting_rod, middle_rod, ending_rod): # specify the start
 
 # function to move the top value of stack 1 to the top of stack 2
 def moveStack(stack1 = stack(), stack2 = stack()):
-    print(f"Starting Rod: {stack1} , Ending Rod: {stack2}")
     temp = stack1.pop()
     stack2.push(temp)
-    print(f"Starting Rod: {stack1} , Ending Rod: {stack2}\
-        \n Moved disk {temp}")
     
 
 ## Okay after consulting chatgpt about this I need to have a function that creates the stacks then a modified version that works with the function I just wrote:
 # will also have to modify the moveStack function to be moveDisk and print what disk is being moved if I want to show the steps.
-
+def moveDisk(stack1 = stack(), stack2 = stack()):
+    #print(f"BEFORE:\nStarting Rod:\n{stack1}\nEnding Rod:\n{stack2}")
+    temp = stack1.pop()
+    stack2.push(temp)
+    print(f"AFTER\nStarting Rod:\n{stack1}\nEnding Rod:\n{stack2}\
+        \n Moved disk {temp}")
 
 # tower of of hanoi recursive solution that works on the stacks 
 def tower_of_hanoi_stacks(n, starting_rod, middle_rod, ending_rod): # specify the starting rods you want
     if n == 1: # Base case
-        moveStack(starting_rod, ending_rod)
+        moveDisk(starting_rod, ending_rod)
     else: # if we are not on the last rod
         tower_of_hanoi_stacks(n-1, starting_rod, ending_rod, middle_rod) # recursive call to move the n-1 disks to the middle rod using the ending rod as the axullary rod
-        moveStack(starting_rod, ending_rod) # move the current disk
+        moveDisk(starting_rod, ending_rod) # move the current disk
         tower_of_hanoi_stacks(n-1, middle_rod, starting_rod, ending_rod) # move the remaining n-1 disks to the ending rod using the starting rod as the auxillary rod
 
+
+# going to need to call the above:
+
+def towergame(n):# calling this towergame for now it is the hanoi stuff with stacks and it will initialize the number of disks
+    # n = number of disks
+    
+    ## initialize the starting, auxillary, and ending rods
+    
+    starting_rod = stack()
+    auxillary_rod = stack()
+    ending_rod = stack()
+    
+    # fill the starting rod
+    for i in range(n,0,-1):
+        starting_rod.push(i)
+    
+    # display starting state
+    print("Start:")
+    print(f"Starting Rod:\n{starting_rod}\nAuxillary Rod:\n{auxillary_rod}\nEnding Rod:\n{ending_rod}")
+    
+    # now we can call the tower of hanoi function.
+    
+    tower_of_hanoi_stacks(n, starting_rod, auxillary_rod, ending_rod)
+    
+    # show the final state
+    print("Ending:")
+    print(f"Starting Rod:\n{starting_rod}\nAuxillary Rod:\n{auxillary_rod}\nEnding Rod:\n{ending_rod}")
 
 '''
 1 mod 3 = 1
