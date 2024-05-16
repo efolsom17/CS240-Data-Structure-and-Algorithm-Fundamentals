4381e4f16beab9af93d0ea06be4d4f5b97a57bb8
efolsom17 <eric.folsom17@gmail.com>
2024-05-16 15:50:10 -0700

    Created the nodes that I will use for this assignment and the data structures assignment.

--- Assignments/Algorithms Assignment 6/Algorithms Assignment 6 - Eric Folsom.py
+++ Assignments/Algorithms Assignment 6/Algorithms Assignment 6 - Eric Folsom.py
@@ -1,5 +1,24 @@
-## Implementing a binary search tree in python
-# Needs in-order, pre-order, post-order traversal
-# Going to need a tree node, similar to the nodes that I created for my linked lists,
-# just instead of prev, and next, i'll hae left and right. SHould probably include tree height just so that I can
-# easily transition it into an AVL tree. I think a lot of this can be done with recursion.
+############### Algorithms Assignment 6 - Eric Folsom ################
+
+
+
+'''
+Implementing a binary search tree in python
+ Needs in-order, pre-order, post-order traversal
+ Going to need a tree node, similar to the nodes that I created for my linked lists,
+ just instead of prev, and next, i'll hae left and right. SHould probably include tree height just so that I can
+ easily transition it into an AVL tree. I think a lot of this can be done with recursion. Might try to do this iteratively as well
+'''
+
+## Tree Node ##
+
+class TreeNode:
+    def __init__(self, data):
+        self.data = data # data we want to add to our tree
+        self.left = None # starting as None for now
+        self.right = None # starting as None for now
+        self.height = 1 # height from that node is 1 by default
+        
+class BinarySearchTree:
+    def __init__(self):
+        pass
