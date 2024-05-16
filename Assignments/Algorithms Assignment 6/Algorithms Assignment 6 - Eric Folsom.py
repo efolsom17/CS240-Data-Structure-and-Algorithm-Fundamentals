5c970fa4ea5f9a65189f3d34dbe1fcfb2df387ed
efolsom17 <eric.folsom17@gmail.com>
2024-05-16 15:44:46 -0700

    Algorithms Assignment 6

---
+++ Assignments/Algorithms Assignment 6/Algorithms Assignment 6 - Eric Folsom.py
@@ -0,0 +1,5 @@
+## Implementing a binary search tree in python
+# Needs in-order, pre-order, post-order traversal
+# Going to need a tree node, similar to the nodes that I created for my linked lists,
+# just instead of prev, and next, i'll hae left and right. SHould probably include tree height just so that I can
+# easily transition it into an AVL tree. I think a lot of this can be done with recursion.
