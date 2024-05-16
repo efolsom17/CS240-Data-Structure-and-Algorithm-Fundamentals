a6468f587bc294d36e4019a9ccab7368c8d278d3
efolsom17 <eric.folsom17@gmail.com>
2024-05-16 16:05:22 -0700

    Think I added the ability to create a tree off of a single point of data or a list.

--- Assignments/Algorithms Assignment 6/Algorithms Assignment 6 - Eric Folsom.py
+++ Assignments/Algorithms Assignment 6/Algorithms Assignment 6 - Eric Folsom.py
@@ -18,7 +18,20 @@ class TreeNode:
         self.left = None # starting as None for now
         self.right = None # starting as None for now
         self.height = 1 # height from that node is 1 by default
+
+
+## Going to need to start with the first data point inserted as the root node, then all the subsequent data
+# inputted as child nodes
+# Something like, if you insert an array, it would choose the middle index of the array to be the root. then assign nodes
+# by the rules of a binary search tree. If you only inputed a single data point, that data would be the root. Or you could make an empty 
+# binary search tree
         
 class BinarySearchTree:
-    def __init__(self):
-        pass
+    def __init__(self, data):
+        # check if data is a single point of data or if it is an array.
+        if data is not list: # if the data that we input is not a list
+            self.root = TreeNode(data) # assign the root as whatever data we inputed
+        else: # if it is a list
+            mid  = len(data)//2
+            self.root = TreeNode(mid)# Assign the middle index to be the root of the binary search tree.
+            
